2fb5e11e6dbc085a931cf541605f4072f2f63596
George Yerontides <georgeyerontides@gmail.com>
2023-02-10 19:06:10 +0200

    Created and cleaned project

---
+++ src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDB Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Just practising NextJs nothing to see here " />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
