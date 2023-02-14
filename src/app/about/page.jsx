72a7ec3eb63997cded35ef0289a7e6fcb178bede
George Yerontides <georgeyerontides@gmail.com>
2023-02-14 18:44:57 +0200

    Minot text edit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className='max-w-6xl mx-auto space-y-4 p-4'>
         <h1 className='text-2xl font-medium text-amber-600 '>About</h1>
-        <p>IMDb is the world's most popular and authoritative source for information on movies, TV shows and celebrities. Products and services to help fans decide what to watch and where to watch it include: the IMDb website for desktop and mobile devices; apps for iOS and Android; and X-Ray on Prime Video. IMDb also produces IMDb original video series and podcasts. For entertainment industry professionals, IMDb provides IMDbPro and Box Office Mojo. IMDb licenses information from its vast and authoritative database to third-party businesses worldwide; learn more at developer.imdb.com. IMDb is an Amazon company. For more information, visit https://www.imdb.com/press and follow @IMDb.</p>
+        <p>IMDb is the world&apos;s most popular and authoritative source for information on movies, TV shows and celebrities. Products and services to help fans decide what to watch and where to watch it include: the IMDb website for desktop and mobile devices; apps for iOS and Android; and X-Ray on Prime Video. IMDb also produces IMDb original video series and podcasts. For entertainment industry professionals, IMDb provides IMDbPro and Box Office Mojo. IMDb licenses information from its vast and authoritative database to third-party businesses worldwide; learn more at developer.imdb.com. IMDb is an Amazon company. For more information, visit https://www.imdb.com/press and follow @IMDb.</p>
     </div>
   )
 }
